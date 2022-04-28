f6913485d47768ceea81c0d5e555bb8fabeb25b0
seunghyun <suji1541@gmail.com>
2022-04-28 14:25:59 +0900

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import '@/styles/globals.css'
+import {Footer, Header, Nav} from "@/components";
+import { wrapper } from '@/modules/store'
+const App = ({ Component, pageProps }) => {
+  return <>
+  <Header/>
+  <Nav/>
+  <Component {...pageProps} />
+  <Footer/>
+  </>
+}
+
+export default wrapper.withRedux(App)
